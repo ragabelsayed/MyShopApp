420c1472330985037dba7a978ba0149bf3304c3a
RGB <ragabelsayed37@gmail.com>
2021-07-07 23:45:28 +0200

    start splash screen & add assets folder

---
+++ my_shop/lib/screens/splash/splash_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../splash/widget/body.dart';
+
+class SplashSCreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
