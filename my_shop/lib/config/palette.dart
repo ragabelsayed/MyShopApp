5e57fe74e6212c1df8e6be7ca97b97b1b9e62638
RGB <ragabelsayed37@gmail.com>
2021-07-08 00:11:27 +0200

    add palette color in conifg file

---
+++ my_shop/lib/config/palette.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const kPrimaryColor = Color(0xFFFF7643);
+  static const kPrimaryLightColor = Color(0xFFFFECDF);
+  static const kPrimaryGradientColor = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+  );
+  static const kSecondaryColor = Color(0xFF979797);
+  static const kTextColor = Color(0xFF757575);
+}
